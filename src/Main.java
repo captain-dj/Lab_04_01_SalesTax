f84b9e5b4de7298228e9352905554a5031ddcee0
captain-dj <whardig10@gmail.com>
2023-09-17 19:09:49 -0400

    V2.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello Your price is 50$ and a a 5% increese");
+
+        double doubleOperandA = 100;
+        double doubleOperandB = 0.05;
+        double doubleOperandC = 50;
+        double doubleSum = 0;
+        double TaxSum = 0;
+        double doubleProduct = 0;
+
+        doubleSum = doubleOperandB / doubleOperandA;
+        TaxSum = doubleOperandC * doubleSum;
+        System.out.println("After the Tax your item is "+ TaxSum +" ");
+
+
     }
 }
